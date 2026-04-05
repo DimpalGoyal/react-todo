93b2e79e8a19dadfa8d833ceed6fef006c7d17a5
Dimpal Goyal <dimpalgoyal129@gmail.com>
2026-04-05 12:02:05 +0000

    finished todo get and post endpoints in index.js

--- backend/index.js
+++ backend/index.js
@@ -6,26 +6,34 @@ const app = express();
 app.use(express.json());
 
 // create a new todo
-app.post("/todo", (req, res) => {
+app.post("/todo", async(req, res) => {
   const createPayload = req.body;
-  const parsedPayload = createTodo.safeParsed(createPayload) 
+  const parsedPayload = createTodo.safeParse(createPayload) 
   if(!parsedPayload.success){
     res.status(403).send({msg: 'invalid input'})
     return
   }
   // store data in mongodb
-  
+    await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false 
+  })
+  res.send({msg: "todo created"})
+
 });
 
 // fetch all todos
-app.get("/todos", (req, res) => {
+app.get("/todos", async(req, res) => {
   // fetch all todos from database
+  const allTodos = await todo.find({}) 
+  res.send({allTodos})
 });
 
 // update the completed field
 app.put("/completed", (req, res) => {
   // change the boolean from false to true for the req id
-  
+
 });
 
 app.listen(8001, () => {
